3dea0dce995d969c71a8db66c3be843cdcf00ba7
vaidika1410 <vaidikakaul20@gmail.com>
2026-02-28 21:25:59 +0530

    typecasting in python

---
+++ typecasting/01_typecasting.py
@@ -0,0 +1,18 @@
+a = 4
+print(a)
+print(type(a))
+
+b = '4'
+print(b)
+print(type(b))
+
+
+# convert b to integer
+c = int(b)
+print(c)
+print(type(c))
+
+d = 223
+e = str(d)
+print(e)
+print(type(e))
